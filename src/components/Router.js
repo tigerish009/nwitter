8a5bfefa191ffb7cd619f9e788b9070b7dc540e7
tigerish009 <tigerish@gmail.com>
2021-07-04 17:15:25 +0900

    const onChange = (event) => {const{target: {name, value },} = event; if(name===email) {setEmail(value);}

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
 
 const AppRouter = ({isLoggedIn}) => {
     return (
