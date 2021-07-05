7cb34e86edb182f1517ff591e42fc2487139dcf4
tigerish009 <tigerish@gmail.com>
2021-07-05 18:05:13 +0900

    const [nweet, setNweet] = useState(), const onChange = (event) => {const {target: {value},}=event; setNweet(value);};

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNweet(value);
+    };
+
+    return (
+        <div>
+            <form onsubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxlength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    )
+}
 
 export default Home;
