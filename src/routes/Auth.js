91ff1ebe70691602db763c928403c1a4003d0a2f
tigerish009 <tigerish@gmail.com>
2021-07-05 14:18:24 +0900

    const [email] = useState, input value={email} onChange={onChange}

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -7,6 +7,7 @@ const Auth = () => {
         const {
             target: { name, value },
         } = event;
+
         if (name === "email") {
             setEmail(value);
         } else if (name === "password") {
@@ -18,21 +19,32 @@ const Auth = () => {
     }
     return (
         <div>
-        <form onSubmit={onSubmit}>
-            <input name="email" type="text" placeholder="Email" required value="email" onChange={onChange} />
-            <input name="password" type="password" placeholder="Password" required value="password" onChange={onChange} />
-            <input type="submit" value="Log In" />
-        </form>
-        <div>
-            <button>Continue with Google </button>
-            <button>Continue with Github </button>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="email" 
+                    type="email" 
+                    placeholder="Email" 
+                    required 
+                    value={email} 
+                    onChange={onChange} 
+                />
+                <input 
+                    name="password" 
+                    type="password" 
+                    placeholder="Password" 
+                    required 
+                    value={password} 
+                    onChange={onChange} 
+                />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google </button>
+                <button>Continue with Github </button>
+            </div>
         </div>
-    </div>
 
     )
 
 }
-
-
-
 export default Auth;
