e3fc2847b15444218c080ca64621d9ccdaf3c79b
tigerish009 <tigerish@gmail.com>
2021-07-05 17:06:12 +0900

    const onLogOutClicks = () => {authService.signOut();

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { authService } from "fBase";
+import { useHistory } from 'react-router-dom';
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+    const history = useHistory();
+    const onLogOutClicks = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    return (
+        <>
+            <button oncClick={onLogOutClicks}>Log Out</button>
+        </>
+    )
+}
 
 export default Profile;
